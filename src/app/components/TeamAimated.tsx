4aee447e7bdffe759937f8f2d39213b6afe2326a
huxaif <shahhuxaif19@gmail.com>
2024-11-22 14:18:33 +0530

    vercel

--- src/app/components/TeamAimated.tsx
+++ src/app/components/TeamAimated.tsx
@@ -37,12 +37,6 @@ const people = [
     image:
       "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
   },
-  {
-    id: 6,
-    name: "Dora",
-    designation: "The Explorer",
-    image: "/public/gatephoto.jpg"
-  },
 ];
 
 export function AnimatedTeam() {
